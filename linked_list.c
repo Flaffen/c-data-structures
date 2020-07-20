f985a7f54822845a0c086d3b8ab5774f86b33475
Stepan Palamarchuk <webocono@gmail.com>
2020-07-20 07:28:01 +0400

    Malloc fun

--- linked_list.c
+++ linked_list.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+/*
+ * A node of a linked list containing an integer as data and a pointer to the next node.
+ * Would be nice to store data of arbitrary type as data.
+ *
+ */
 struct node {
 	int data;
 	struct node *next;
 };
 
+/*
+ * Linked list containing head node pointer. Clunky, don't know if it's the right way to initialize a linked list.
+ *
+ */
 struct list {
 	struct node *head;
 };
 
+/*
+ * Print the 'data' field of every node of a linked list.
+ *
+ */
 void print_data(struct list *l)
 {
 	struct node *current = l->head;
+
+	if (current == NULL) return;
 
 	do {
 		printf("current->data is %d\n", current->data);
@@ -19,6 +35,10 @@ void print_data(struct list *l)
 	} while (current != NULL);
 }
 
+/*
+ * Add a node at the end of a linked list.
+ *
+ */
 void add_node(struct list *l, struct node *n)
 {
 	if (l->head == NULL) {
@@ -32,12 +52,20 @@ void add_node(struct list *l, struct node *n)
 	}
 }
 
+/*
+ * Create a new linked list node and return it as value. Need to find out how to use malloc() and the whole memory management stuff to return a pointer.
+ *
+ */
 struct node create_node(int data) {
 	struct node new_node = { data, NULL };
 
 	return new_node;
 }
 
+/*
+ * Create a linked list with 'head' as head pointer.
+ *
+ */
 struct list create_list(struct node *head)
 {
 	struct list new_list = { head };
@@ -45,6 +73,10 @@ struct list create_list(struct node *head)
 	return new_list;
 }
 
+/*
+ * Insert a node after a node containing 'd' in its 'data' field.
+ *
+ */
 int insert_after(struct list *l, int d, struct node *n)
 {
 	struct node *current = l->head;
@@ -81,35 +113,55 @@ int delete(struct list *l, int d)
 		} else {
 			prev->next = current->next;
 		}
+		free(current);
 		return 0;
 	}
 }
 
+struct node *create_node_malloc(int d)
+{
+	struct node *p;
+	p = malloc(sizeof *p);
+	p->data = d;
+
+	return p;
+}
+
+struct list *create_list_malloc(struct node *head)
+{
+	struct list *l;
+	l = malloc(sizeof *l);
+
+	l->head = head;
+
+	return l;
+}
+
+void print_node_data(struct node *node)
+{
+	printf("%d\n", (*(node)).data);
+}
+
 int main(void)
 {
-	struct node n0 = create_node(0);
+	struct list *llist = create_list_malloc(NULL);
 
-	struct list linked_list = create_list(&n0); 
-
-	struct node n1 = create_node(10);
-	struct node n2 = create_node(20);
-
-	add_node(&linked_list, &n1);
-	add_node(&linked_list, &n2);
-
-	struct list *pll = &linked_list;
-
-	struct node n3 = create_node(100);
-	if (insert_after(pll, 20, &n3)) {
-		printf("did not find\n");
+	// Allocate memory for 10 node structs and fill out the linked list with them.
+	for (int i = 0; i < 10; i++) {
+		add_node(llist, create_node_malloc(i + 1));
 	}
 
-	struct node n4 = create_node(9000);
-	insert_after(pll, 20, &n4);
+	// Do things with the linked list.
+	print_data(llist);
 
-	delete(pll, 20);
+	// Deallocate memory for the linked list.
+	struct node *current = llist->head;
 
-	print_data(pll);
+	while (current != NULL) {
+		delete(llist, current->data);
+		current = current->next;
+	}
+	free(llist);
 
 	return 0;
 }
